ab4b11989b1295ff1e651951080f9433e4f05e5d
CharlilouM <charlilou.maes@gmail.com>
2024-10-25 14:47:32 +0200

    affichage objet en cours de craft

--- craft.c
+++ craft.c
@@ -1,6 +1,10 @@
 #include "craft.h"
+
 int CraftSelectedItem=-1;
-bool DropDownMenu = false;
+
+CraftMenu CurrentMenu=NONE;
+Craft BaseCraftInvent[MaxBaseCraft];
+
 
 void CraftButton() {
     BaseCraftButton = (Rectangle) { GetScreenWidth()/2 - 350, 100, 100, 50 };
@@ -18,9 +22,10 @@ void DrawCraftMenu() {
     // Dessiner le bouton principal du menu déroulant
     if (CheckCollisionPointRec(GetMousePosition(), BaseCraftButton)) {
         DrawRectangleRec(BaseCraftButton, LIGHTGRAY); // Couleur si survolé
-        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-            DropDownMenu = !DropDownMenu;  // Ouvrir/fermer le menu
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && CurrentMenu==NONE) {
+            CurrentMenu=BASE;  // Ouvrir/fermer le menu
         }
+        else if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) CurrentMenu =NONE;
     } else {
         DrawRectangleRec(BaseCraftButton, GRAY);
     }
@@ -28,13 +33,64 @@ void DrawCraftMenu() {
     DrawText("Base", BaseCraftButton.x + 10, BaseCraftButton.y + 10, 20, WHITE);
 
     // Si le menu déroulant est ouvert
-    if (DropDownMenu) {
-        for (int i = 0; i < MaxBaseCraft; i++) {
+    if (CurrentMenu==BASE) {
+        DrawBaseCraft();
+    }
+    if (CurrentMenu!=NONE) {
+      CraftSelectedItem=-1;
+    }
+    // Afficher l'option sélectionnée
+    if (CraftSelectedItem >= 0) {
+        DrawCraftItem();
+}
+}
+void DrawCraftItem(){
+    DrawText(TextFormat("Selected: Option %d", CraftSelectedItem + 1), 400, 180, 20, DARKGREEN);
+    Vector2 pos = { 400, 200 };
+    DrawTextureEx(BaseCraftInvent.texture, pos,  0.0f,  8.0f, WHITE);
+
+    ConfirmCraftButton1 = (Rectangle) { 300, 400, 120, 50 };
+    ConfirmCraftButton5 = (Rectangle) { 450, 400, 120, 50 };
+    if (CheckCollisionPointRec(GetMousePosition(), ConfirmCraftButton1)) {
+        DrawRectangleRec(ConfirmCraftButton1, LIGHTGRAY); // Couleur si survolé
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && CurrentMenu==NONE) {
+        }
+        else if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) ;
+    } else {
+        DrawRectangleRec(ConfirmCraftButton1, GRAY);
+    }
+    DrawText(TextFormat("Fabriquer x1"), ConfirmCraftButton1.x + 10, ConfirmCraftButton1.y + 10, 15, DARKGRAY);
+
+
+    if (CheckCollisionPointRec(GetMousePosition(), ConfirmCraftButton5)) {
+            DrawRectangleRec(ConfirmCraftButton5, LIGHTGRAY); // Couleur si survolé
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && CurrentMenu==NONE) {
+            }
+            else if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) ;
+        } else {
+            DrawRectangleRec(ConfirmCraftButton5, GRAY);
+        }
+    DrawText(TextFormat("Fabriquer x5"), ConfirmCraftButton5.x + 10, ConfirmCraftButton5.y + 10, 15, DARKGRAY);
+
+}
+
+
+void InitBaseCraft() {
+    BaseCraftInvent[0] = (Craft) { copperTexture, 2 };
+
+    // Initialiser les rectangles des options de menu
+    for (int i = 0; i < MaxBaseCraft; i++) {
+        dropdownMenu[i] = (Rectangle) { screenWidth-(screenWidth-100), 150 + (i + 1) * 50, 200, 40 };
+    }
+}
+
+void DrawBaseCraft() {
+    for (int i = 0; i < MaxBaseCraft; i++) {
             if (CheckCollisionPointRec(GetMousePosition(), dropdownMenu[i])) {
                 DrawRectangleRec(dropdownMenu[i], LIGHTGRAY);  // Survolé
                 if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
                     CraftSelectedItem = i;  // Option sélectionnée
-                    DropDownMenu = false;  // Fermer le menu après sélection
+                    CurrentMenu=NONE;  // Fermer le menu après sélection
                 }
             } else {
                 DrawRectangleRec(dropdownMenu[i], GRAY);
@@ -42,34 +98,6 @@ void DrawCraftMenu() {
 
             DrawText(TextFormat("Option %d", i + 1), dropdownMenu[i].x + 10, dropdownMenu[i].y + 10, 20, DARKGRAY);
         }
-    }
-
-    // Afficher l'option sélectionnée
-    if (CraftSelectedItem >= 0) {
-        DrawText(TextFormat("Selected: Option %d", CraftSelectedItem + 1), 100, 400, 20, DARKGREEN);
-    }
-}
-
-void InitBaseCraft() {
-    // Remplir BaseCraft avec des textures fictives (à remplacer par vos vraies textures)
-    for (int i = 0; i < MaxBaseCraft; i++) {
-        //BaseCraft[i] = (Craft) { LoadTexture("path_to_texture.png"), 1 };
-    }
-
-    // Initialiser les rectangles des options de menu
-    for (int i = 0; i < MaxBaseCraft; i++) {
-        dropdownMenu[i] = (Rectangle) { BaseCraftButton.x, BaseCraftButton.y + (i + 1) * 50, 200, 40 };
-    }
-}
-
-void DrawBaseCraft() {
-    // Dessiner les options si le menu est ouvert
-    if (DropDownMenu) {
-        for (int i = 0; i < MaxBaseCraft; i++) {
-            DrawRectangleRec(dropdownMenu[i], LIGHTGRAY);
-            DrawText(TextFormat("Item %d", i + 1), dropdownMenu[i].x + 10, dropdownMenu[i].y + 10, 20, DARKGRAY);
-        }
-    }
 
     
 }
--- craft.h
+++ craft.h
@@ -10,16 +10,20 @@ void CraftButton();
 void DrawCraftMenu();
 void DrawBaseCraft();
 void InitBaseCraft();
+void DrawCraftItem();
 
 extern bool DropDownMenu;
+typedef enum CraftMenu { NONE,BASE } CraftMenu;
+CraftMenu CurrentMenu;
+
 
 typedef struct Craft {
     Texture2D texture;
     int quantity;
 } Craft;
 
-Craft BaseCraft[MaxBaseCraft];
+extern Craft BaseCraftInvent[MaxBaseCraft];
 
-Rectangle BaseCraftButton, dropdownMenu[MaxBaseCraft];
+Rectangle BaseCraftButton,ConfirmCraftButton1,ConfirmCraftButton5, dropdownMenu[MaxBaseCraft];
 extern int CraftSelectedItem;
 #endif
