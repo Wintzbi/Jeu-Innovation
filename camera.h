5c095a2c6b2c3302e90e807de8571090dbc4d635
Wintzbi <leynaickd@gmail.com>
2024-10-25 08:16:52 +0200

    création de fichier camera

--- camera.h
+++ camera.h
@@ -3,17 +3,5 @@
 
 #include "raylib.h"
 
-// Taille de la zone visible par la caméra
-extern const int visibleCols;
-extern const int visibleRows;
-
-// Fonction pour mettre à jour la caméra
-void UpdateTheCamera();
-
-// Fonction pour dessiner la carte avec la caméra
-void DrawMap();
-
-// Fonction pour gérer la mise à jour et le dessin de la caméra
-void CameraUpdateAndDraw();
 
 #endif
